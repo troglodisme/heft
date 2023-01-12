1f3023e7e0c5a967f5560a2f889beda50ef1a534
troglodisme <giulio.ammendola@network.rca.ac.uk>
2023-01-13 00:11:50 +0100

    Initial Commit

---
+++ heft/heftApp.swift
@@ -0,0 +1,17 @@
+//
+//  heftApp.swift
+//  heft
+//
+//  Created by Giulio on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct heftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
